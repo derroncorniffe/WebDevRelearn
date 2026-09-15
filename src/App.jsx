72cf59d59f275ca230dfccbd0947cd6d638b2dcb
derroncorniffe <corniffed@gmail.com>
2026-09-15 12:37:46 -0500

    Added Programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/programs/Programs";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 };
