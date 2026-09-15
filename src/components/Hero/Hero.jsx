092bd0c7a1df34904ed9b66c3cbbecefd0bf0ea8
derroncorniffe <corniffed@gmail.com>
2026-09-15 11:51:16 -0500

    Added Navbar and Background

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>We ensure better education for a better world</h1>
+        <p>Our</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
