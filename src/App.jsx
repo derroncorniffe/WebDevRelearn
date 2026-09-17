d6f808057b84b84cb373c549bd0c3a1b9e51bc9b
derroncorniffe <corniffed@gmail.com>
2026-09-16 22:54:55 -0500

    Added video player, mobile layout, and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/programs/Programs";
@@ -7,8 +7,12 @@ import About from "./components/About/About";
 import Campus from "./components/Campus/Campus";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -16,14 +20,16 @@ const App = () => {
       <div className="container">
         <Title subtitle="Our Program" title="What We Offer" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subtitle="Gallery" title="Campus Photos" />
         <Campus />
         <Title subtitle="TESTIMONIALS" title="What Students Say" />
         <Testimonials />
         <Title subtitle="Contact Us" title="Get in Touch" />
         <Contact />
+        <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
+import menu_icon from "../../assets/menu-icon.png";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -11,20 +13,59 @@ const Navbar = () => {
     });
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
+
   return (
     <div className={`nav-outer ${sticky ? "dark-nav" : ""}`}>
       <nav className="container">
         <img src={logo} alt="" className="logo" />
-        <ul>
-          <li>Home</li>
-          <li>Programs</li>
-          <li>About us</li>
-          <li>Campus</li>
-          <li>Testimonials</li>
+        <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
           <li>
-            <button className="btn">Contact us</button>
+            <Link to="hero" smooth={true} offset={0} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="programs" smooth={true} offset={-260} duration={500}>
+              Programs
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} offset={-150} duration={500}>
+              About us
+            </Link>
+          </li>
+          <li>
+            <Link to="campus" smooth={true} offset={-260} duration={500}>
+              Campus
+            </Link>
+          </li>
+          <li>
+            <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+              Testimonials
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-260}
+              duration={500}
+              className="btn"
+            >
+              Contact us
+            </Link>
           </li>
         </ul>
+        <img
+          src={menu_icon}
+          alt=""
+          className="menu-icon"
+          onClick={toggleMenu}
+        />
       </nav>
     </div>
   );
